0194d0dc73c9d0a4e777169c20401df9c900d63b
jatin kukreja <jatinkukreja18@yahoo.in>
2020-11-18 19:27:31 +0530

    name correctiong

--- CBI/brochure/javascript/config.js
+++ CBI/brochure/javascript/config.js
@@ -6,7 +6,7 @@ var bookConfig = {
    "appLogoLinkURL": "http:\/\/sbicard.com",
    "HomeURL": "",
    "appLogoOpenWindow": "Blank",
-   "bookTitle": "CBI SimplySave SBI Card",
+   "bookTitle": "Central Bank Of India SimplySave SBI Card",
    "bookDescription": "",
    "toolbarColor": "#181818",
    "iconColor": "#eeeeee",
